6ec12cabb66b99898478deaf0222ef4a878356b4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-29 11:41:15 -0300

    Saved sara/main.py at Timestamp: 2018-06-29 14:41:15

--- sara/main.py
+++ sara/main.py
@@ -4,4 +4,5 @@ castelo = "https://upload.wikimedia.org/wikipedia/commons/8/88/Castelo_de_Guimar
 carro = "http://www.vpowerautomoveis.com.br/home/imgs/carro_top4.png"
 # cheguei
 def historia():
+     cenacastelo = Cena(img = "https://upload.wikimedia.org/wikipedia/commons/8/88/Castelo_de_Guimar%C3%A3es_%28Portugal%29.jpg")
 historia()
