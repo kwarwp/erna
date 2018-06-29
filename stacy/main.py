185d71ed840d9dce9be480dd5dc7511899a5d057
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-29 11:52:23 -0300

    Saved stacy/main.py at Timestamp: 2018-06-29 14:52:23

--- stacy/main.py
+++ stacy/main.py
@@ -4,6 +4,6 @@ ceu = "https://png.pngtree.com/thumb_back/fw800/back_pic/03/56/29/24579dee6c24b4
 paris = "http://www.freepngimg.com/download/paris/24125-5-paris-picture.png"
 #Bonne nuit
 def historia():
-     cenaceu = Cena(imgn = "https://png.pngtree.com/thumb_back/fw800/back_pic/03/56/29/24579dee6c24b45.jpg")
+     cenaceu = Cena(img = "https://png.pngtree.com/thumb_back/fw800/back_pic/03/56/29/24579dee6c24b45.jpg")
      cenaceu.vai()
 historia()
