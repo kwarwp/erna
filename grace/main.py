2654af18ec46e7f6e717b0e4907e3b63886f15f8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-29 11:41:25 -0300

    Saved grace/main.py at Timestamp: 2018-06-29 14:41:25

--- grace/main.py
+++ grace/main.py
@@ -3,3 +3,6 @@ from _spy.vittolino.main import Cena, Elemento, Texto
 floresta = "http://www.ecodesenvolvimento.org/posts/2013/maio/produtos-das-florestas-podem-ser-solucao-para/images/floresta.jpg"
 ogro = "https://i.pinimg.com/originals/af/b6/b3/afb6b390ccd6d1415c72f98d6db91ccd.png"
 #Vou te matar e comer sua carne com vinho! Muahahaha
+def historia():
+     Cenafloresta = cena(img = "http://www.ecodesenvolvimento.org/posts/2013/maio/produtos-das-florestas-podem-ser-solucao-para/images/floresta.jpg"
+historia()
