af36e9b2297c9adc7a329c421ccf0ce7359a5cba
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-29 11:46:56 -0300

    Saved angie/__log__.py at Timestamp: 2018-06-29 14:46:55

--- angie/__log__.py
+++ angie/__log__.py
@@ -17,3 +17,21 @@
     cenacidade = Cena(img = "https://media.wsimag.com/attachments/87d7e431ded78e4f143c7e84db3e01a694a8276e/store/fill/1020/574/cc1c81d29f191e0dd08e953179b0d2d3baa88ab791485adf5c60ca97fdd2/Chongqing-China-Tempestade-sobre-a-cidade.jpg")
 TypeError: 'module' object is not callable
 '''},
+{'date': 'Fri Jun 29 2018 11:46:57.264 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 9
+    historia()
+  module <module> line 7
+    cenacidade = Cena(img = "https://media.wsimag.com/attachments/87d7e431ded78e4f143c7e84db3e01a694a8276e/store/fill/1020/574/cc1c81d29f191e0dd08e953179b0d2d3baa88ab791485adf5c60ca97fdd2/Chongqing-China-Tempestade-sobre-a-cidade.jpg")
+TypeError: 'module' object is not callable
+'''},
