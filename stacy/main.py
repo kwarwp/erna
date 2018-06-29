49a09892ae48ef89379334d4beab16c2f0abc607
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-29 11:41:12 -0300

    Saved stacy/main.py at Timestamp: 2018-06-29 14:41:11

--- stacy/main.py
+++ stacy/main.py
@@ -1,4 +1,9 @@
 from _spy.vittolino.main import Cena, Elemento, Texto
 #Tassiane 
-céu = "https://png.pngtree.com/thumb_back/fw800/back_pic/03/56/29/24579dee6c24b45.jpg"
+ceu = "https://png.pngtree.com/thumb_back/fw800/back_pic/03/56/29/24579dee6c24b45.jpg"
 paris = "http://www.freepngimg.com/download/paris/24125-5-paris-picture.png"
+#Bonne nuit
+def historia():
+     cenaCeu = Cena(imgn = "https://png.pngtree.com/thumb_back/fw800/back_pic/03/56/29/24579dee6c24b45.jpg"
+
+historia()
