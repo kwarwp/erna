c6c1ec0c5185db9512256c4ea4928266cc08f403
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-29 11:54:52 -0300

    Saved grace/__log__.py at Timestamp: 2018-06-29 14:54:52

--- grace/__log__.py
+++ grace/__log__.py
@@ -84,3 +84,21 @@ TypeError: 'module' object is not callable
     cenafloresta = Cena(img = "http://www.ecodesenvolvimento.org/posts/2013/maio/produtos-das-florestas-podem-ser-solucao-para/images/floresta.jpg")
 TypeError: 'module' object is not callable
 '''},
+{'date': 'Fri Jun 29 2018 11:54:53.294 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 9
+    historia()
+  module <module> line 7
+    cenafloresta = Cena(img = "http://www.ecodesenvolvimento.org/posts/2013/maio/produtos-das-florestas-podem-ser-solucao-para/images/floresta.jpg")
+TypeError: 'module' object is not callable
+'''},
