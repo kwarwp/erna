d0dfac735fcda9c6e66527a7c0e9414a2f80fab9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-29 11:43:52 -0300

    Created kellee/__log__.py at Timestamp: 2018-06-29 14:43:52

---
+++ kellee/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Fri Jun 29 2018 11:43:50.925 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 7
+  Cenafloresta = Cena(ing = "http://2.bp.blogspot.com/-QfJCgBGjGLI/U7sgKKLbRWI/AAAAAAAAZEE/RXU-xgjdPlg/s1600/ZQHFqWr.jpg"
+                                                                                                                           ^
+SyntaxError: invalid syntax
+'''},
