c013351f2a69afee63503fdfccde22b01de721e1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-29 11:20:51 -0300

    Created angie/main.py at Timestamp: 2018-06-29 14:20:51

---
+++ angie/main.py
@@ -0,0 +1,4 @@
+from _spy.vittolino.main import Cena, Elemento, Texto
+#Cibele
+cidade = "https://media.wsimag.com/attachments/87d7e431ded78e4f143c7e84db3e01a694a8276e/store/fill/1020/574/cc1c81d29f191e0dd08e953179b0d2d3baa88ab791485adf5c60ca97fdd2/Chongqing-China-Tempestade-sobre-a-cidade.jpg"
+aviao = "http://i.imgur.com/geBGkhx.png"
