a8b6391fce9997ef03c6504a9c76ce64b9529cf2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-29 11:49:29 -0300

    Saved kellee/__log__.py at Timestamp: 2018-06-29 14:49:28

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -13,3 +13,10 @@ SyntaxError: invalid syntax
                                                                                                                                 ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Fri Jun 29 2018 11:49:29.220 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 7
+  Cenafloresta = Cena(ing = "http://2.bp.blogspot.com/-QfJCgBGjGLI/U7sgKKLbRWI/AAAAAAAAZEE/RXU-xgjdPlg/s1600/ZQHFqWr.jpg"
+                                                                                                                                ^
+SyntaxError: invalid syntax
+'''},
