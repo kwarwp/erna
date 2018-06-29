0f1f160180590ab5e5ed87c903be2e77b87ffd25
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-29 11:41:27 -0300

    Saved roxanne/main.py at Timestamp: 2018-06-29 14:41:26

--- roxanne/main.py
+++ roxanne/main.py
@@ -4,4 +4,5 @@ tristeza = "http://f.i.uol.com.br/fotografia/2016/05/02/606604-970x600-1.jpg"
 ferro "https://png.pngtree.com/element_pic/16/12/27/f8d3f37ac40656a40836942a56f8fa53.jpg"
 #um carro bateu em um pilar de ferro e esposa ficou muito triste
 def historia():
+       cenatristreza = Cena(img = "http://f.i.uol.com.br/fotografia/2016/05/02/606604-970x600-1.jpg")
 historia()
