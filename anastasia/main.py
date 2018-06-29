8584bc592d35095b8435849ac8c787e965658be5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-29 11:49:37 -0300

    Saved anastasia/main.py at Timestamp: 2018-06-29 14:49:36

--- anastasia/main.py
+++ anastasia/main.py
@@ -1,8 +1,9 @@
-from _spy.vittolino.main import Cena, Elemento, Texto
+from _spy.vitollino.main import Cena, Elemento, Texto
 # Raphael
 oceano = "https://mardehistorias.files.wordpress.com/2010/11/oceano.jpg"
 nemo = "http://www.araguaiaaquarios.com.br/loja/modules/possequence/images/image_1.jpg"
 #Glub Glub
 def historia():
    cenaoceano = Cena(img = "https://mardehistorias.files.wordpress.com/2010/11/oceano.jpg")
+   cenaoceano.vai()
 historia()
