b3ce77eb11ae01567d5dbe5e78377c7b6f82e8d8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-29 11:41:10 -0300

    Saved alexa/main.py at Timestamp: 2018-06-29 14:41:09

--- alexa/main.py
+++ alexa/main.py
@@ -2,3 +2,7 @@
 Alexandre
 praia = "https://beachpark.com.br/mobile/wp-content/themes/beachpark/img/praia/apresentacao.jpg"
 pessoa = "http://www.trackandfield.com.br/wordpress/wp-content/uploads/2014/10/Screen-Shot-2014-10-15-at-4.38.02-PM.png"
+# bom dia 
+def historia():
+     cenapraia = Cena(ing =  "https://beachpark.com.br/mobile/wp-content/themes/beachpark/img/praia/apresentacao.jpg")
+historia()
