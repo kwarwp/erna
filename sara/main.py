890baeec8affaf28257855f89ba5d415728309b7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-29 11:35:32 -0300

    Saved sara/main.py at Timestamp: 2018-06-29 14:35:32

--- sara/main.py
+++ sara/main.py
@@ -3,3 +3,5 @@ from _spy.vittolino.main import Cena, Elemento, Texto
 castelo = "https://upload.wikimedia.org/wikipedia/commons/8/88/Castelo_de_Guimar%C3%A3es_%28Portugal%29.jpg"
 carro = "http://www.vpowerautomoveis.com.br/home/imgs/carro_top4.png"
 # cheguei
+def historia():
+historia()
