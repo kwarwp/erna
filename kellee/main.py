862d33d2faa8d94b0761fd37c48c9310eb795d8d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-29 11:49:25 -0300

    Saved kellee/main.py at Timestamp: 2018-06-29 14:49:25

--- kellee/main.py
+++ kellee/main.py
@@ -1,4 +1,4 @@
-from _spy.vittolino.main import Cena, Elemento, Texto
+from _spy.vitollino.main import Cena, Elemento, Texto
 #Matheus
 FLORESTA = "http://2.bp.blogspot.com/-QfJCgBGjGLI/U7sgKKLbRWI/AAAAAAAAZEE/RXU-xgjdPlg/s1600/ZQHFqWr.jpg"
 hokage = "https://i.pinimg.com/originals/39/02/44/390244b135c00088e1b8b7871d45957a.png"
