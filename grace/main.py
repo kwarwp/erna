87323a7c03dce5210ac43dbe0ed12d59df19d15e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-29 11:20:48 -0300

    Saved grace/main.py at Timestamp: 2018-06-29 14:20:48

--- grace/main.py
+++ grace/main.py
@@ -1,2 +1,4 @@
-# erna.grace.main.py
-# test
+from _spy.vittolino.main import Cena, Elemento, Texto
+# Feliphe
+floresta = "http://www.ecodesenvolvimento.org/posts/2013/maio/produtos-das-florestas-podem-ser-solucao-para/images/floresta.jpg"
+Ogro = "https://i.pinimg.com/originals/af/b6/b3/afb6b390ccd6d1415c72f98d6db91ccd.png"
