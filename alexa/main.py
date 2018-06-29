b70a7af5e7444e47b7bd991e3f0fae6aeee1542b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-29 11:29:53 -0300

    Saved alexa/main.py at Timestamp: 2018-06-29 14:29:53

--- alexa/main.py
+++ alexa/main.py
@@ -1,4 +1,4 @@
 # erna.alexa.main.py
 Alexandre
 praia = "https://beachpark.com.br/mobile/wp-content/themes/beachpark/img/praia/apresentacao.jpg"
-pessoa = "
+pessoa = "http://www.trackandfield.com.br/wordpress/wp-content/uploads/2014/10/Screen-Shot-2014-10-15-at-4.38.02-PM.png"
