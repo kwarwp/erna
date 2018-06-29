552d5566609f50ffe92412995055d747de8bc03a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-29 11:42:44 -0300

    Created grace/__log__.py at Timestamp: 2018-06-29 14:42:44

---
+++ grace/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Fri Jun 29 2018 11:42:44.411 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 7
+  Cenafloresta = cena(img = "http://www.ecodesenvolvimento.org/posts/2013/maio/produtos-das-florestas-podem-ser-solucao-para/images/floresta.jpg"
+                                                                                                                                                   ^
+SyntaxError: invalid syntax
+'''},
