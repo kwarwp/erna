339a5bc7ea9fe01532773b3bd9f698b2db26a858
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-29 11:30:06 -0300

    Created libby/main.py at Timestamp: 2018-06-29 14:30:05

---
+++ libby/main.py
@@ -0,0 +1,5 @@
+from _spy.vittolino.main import Cena, Elemento, Texto
+# Lana
+lugar = "https://media-cdn.tripadvisor.com/media/photo-s/12/f8/68/3d/big-bus-paris-hop-on.jpg"
+passaros = "https://i.pinimg.com/originals/18/80/5f/18805f2560e462573fee515cfe9eb364.jpg"
+#piupiupiupiu
