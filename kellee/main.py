f8503069847ee1d64bbee21c61c8e0e85a21ca49
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-29 11:43:43 -0300

    Saved kellee/main.py at Timestamp: 2018-06-29 14:43:43

--- kellee/main.py
+++ kellee/main.py
@@ -2,3 +2,7 @@ from _spy.vittolino.main import Cena, Elemento, Texto
 #Matheus
 FLORESTA = "http://2.bp.blogspot.com/-QfJCgBGjGLI/U7sgKKLbRWI/AAAAAAAAZEE/RXU-xgjdPlg/s1600/ZQHFqWr.jpg"
 hokage = "https://i.pinimg.com/originals/39/02/44/390244b135c00088e1b8b7871d45957a.png"
+#Olá eu sou o kakashi, o sexto hokage da aldeia da folha
+def historia ():
+     Cenafloresta = Cena(ing = "http://2.bp.blogspot.com/-QfJCgBGjGLI/U7sgKKLbRWI/AAAAAAAAZEE/RXU-xgjdPlg/s1600/ZQHFqWr.jpg"
+historia()
