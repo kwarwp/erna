2dec59f623f1aa87c945a2abfb5ce19803947c95
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-29 11:41:45 -0300

    Created stacy/__log__.py at Timestamp: 2018-06-29 14:41:45

---
+++ stacy/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Fri Jun 29 2018 11:41:45.148 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 7
+  cenaCeu = Cena(imgn = "https://png.pngtree.com/thumb_back/fw800/back_pic/03/56/29/24579dee6c24b45.jpg"
+                                                                                                           ^
+SyntaxError: invalid syntax
+'''},
