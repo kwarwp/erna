4c4891b26146a864ea72d44c0858a60422ee2d28
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-29 11:52:22 -0300

    Saved grace/main.py at Timestamp: 2018-06-29 14:52:18

--- grace/main.py
+++ grace/main.py
@@ -4,6 +4,6 @@ floresta = "http://www.ecodesenvolvimento.org/posts/2013/maio/produtos-das-flore
 ogro = "https://i.pinimg.com/originals/af/b6/b3/afb6b390ccd6d1415c72f98d6db91ccd.png"
 #Vou te matar e comer sua carne com vinho! Muahahaha
 def historia():
-     cenafloresta = Cena(img = "http://www.ecodesenvolvimento.org/posts/2013/maio/produtos-das-florestas-podem-ser-solucao-para/images/floresta.jpg"
-     cenafloresta.vai()
+   cenafloresta = Cena(img = "http://www.ecodesenvolvimento.org/posts/2013/maio/produtos-das-florestas-podem-ser-solucao-para/images/floresta.jpg"
+   cenafloresta.vai()
 historia()
