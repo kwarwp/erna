6fb4441740b65913c3d774ff2807af8162ff4074
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-29 11:49:23 -0300

    Saved sara/main.py at Timestamp: 2018-06-29 14:49:23

--- sara/main.py
+++ sara/main.py
@@ -1,4 +1,4 @@
-from _spy.vittolino.main import Cena, Elemento, Texto
+from _spy.vitollino.main import Cena, Elemento, Texto
 # Luciane
 castelo = "https://upload.wikimedia.org/wikipedia/commons/8/88/Castelo_de_Guimar%C3%A3es_%28Portugal%29.jpg"
 carro = "http://www.vpowerautomoveis.com.br/home/imgs/carro_top4.png"
