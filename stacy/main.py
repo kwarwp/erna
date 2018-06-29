df6495cad64dfc968ff14a8705e9ad2906c47387
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-29 11:30:03 -0300

    Created stacy/main.py at Timestamp: 2018-06-29 14:30:03

---
+++ stacy/main.py
@@ -0,0 +1,4 @@
+from _spy.vittolino.main import Cena, Elemento, Texto
+#Tassiane 
+céu = "https://png.pngtree.com/thumb_back/fw800/back_pic/03/56/29/24579dee6c24b45.jpg"
+paris = "http://www.freepngimg.com/download/paris/24125-5-paris-picture.png"
