2b8f33b1f240ea76435398a18edd5c1fe8a69e64
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-29 11:51:38 -0300

    Saved kellee/main.py at Timestamp: 2018-06-29 14:51:37

--- kellee/main.py
+++ kellee/main.py
@@ -4,6 +4,6 @@ FLORESTA = "http://2.bp.blogspot.com/-QfJCgBGjGLI/U7sgKKLbRWI/AAAAAAAAZEE/RXU-xg
 hokage = "https://i.pinimg.com/originals/39/02/44/390244b135c00088e1b8b7871d45957a.png"
 #Olá eu sou o kakashi, o sexto hokage da aldeia da folha
 def historia ():
-     cenafloresta = Cena(img = "http://2.bp.blogspot.com/-QfJCgBGjGLI/U7sgKKLbRWI/AAAAAAAAZEE/RXU-xgjdPlg/s1600/ZQHFqWr.jpg"
-     cenafloresta.vai()
+     Cenafloresta = Cena(img = "http://2.bp.blogspot.com/-QfJCgBGjGLI/U7sgKKLbRWI/AAAAAAAAZEE/RXU-xgjdPlg/s1600/ZQHFqWr.jpg"
+     Cenafloresta.vai()
 historia()
