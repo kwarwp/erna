1b88c6db536ffba75e00d897aa76f5f168d19604
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-29 11:50:19 -0300

    Saved alexa/main.py at Timestamp: 2018-06-29 14:50:19

--- alexa/main.py
+++ alexa/main.py
@@ -1,5 +1,5 @@
-# erna.alexa.main.py
-Alexandre
+from _spy.vitollino.main import Cena, Elemento, Texto
+#Alexandre
 praia = "https://beachpark.com.br/mobile/wp-content/themes/beachpark/img/praia/apresentacao.jpg"
 pessoa = "http://www.trackandfield.com.br/wordpress/wp-content/uploads/2014/10/Screen-Shot-2014-10-15-at-4.38.02-PM.png"
 # bom dia 
