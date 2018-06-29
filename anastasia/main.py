19ec28c298670da7e1cfb18dd9bbaa4fe1713987
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-29 11:20:52 -0300

    Saved anastasia/main.py at Timestamp: 2018-06-29 14:20:52

--- anastasia/main.py
+++ anastasia/main.py
@@ -1,2 +1,4 @@
-# erna.anastasia.main.py
-Raphael
+from _spy.vittolino.main import Cena, Elemento, Texto
+# Raphaelhttps://mardehistorias.files.wordpress.com/2010/11/oceano.jpg
+oceano = "https://mardehistorias.files.wordpress.com/2010/11/oceano.jpg"
+nemo = "http://www.araguaiaaquarios.com.br/loja/modules/possequence/images/image_1.jpg"
