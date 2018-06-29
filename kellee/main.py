4409ab1fe71ce6a726bd1f69f82b3d6eaa92f719
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-29 11:46:11 -0300

    Saved kellee/main.py at Timestamp: 2018-06-29 14:46:11

--- kellee/main.py
+++ kellee/main.py
@@ -5,4 +5,5 @@ hokage = "https://i.pinimg.com/originals/39/02/44/390244b135c00088e1b8b7871d4595
 #Olá eu sou o kakashi, o sexto hokage da aldeia da folha
 def historia ():
      Cenafloresta = Cena(ing = "http://2.bp.blogspot.com/-QfJCgBGjGLI/U7sgKKLbRWI/AAAAAAAAZEE/RXU-xgjdPlg/s1600/ZQHFqWr.jpg"
+     cenafloresta.vai()
 historia()
