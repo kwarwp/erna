76b38acc94d3c005923aced77f36690300f214fa
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-06 10:33:54 -0300

    Saved sara/main.py at Timestamp: 2018-07-06 13:33:54

--- sara/main.py
+++ sara/main.py
@@ -5,7 +5,7 @@ linkcarro = "http://www.vpowerautomoveis.com.br/home/imgs/carro_top4.png"
 # cheguei
 def historia():
      cenacastelo = Cena(img = "https://upload.wikimedia.org/wikipedia/commons/8/88/Castelo_de_Guimar%C3%A3es_%28Portugal%29.jpg")
-     carro = Elemento(img = linkcarro, tit = "Carro", style = dict (top = 80, left = 250, height = 200, width = 100))
+     carro = Elemento(img = linkcarro, tit = "Carro", style = dict (top = 60, left = 150, height = 200, width = 100))
      carro.entra(cenacastelo)
      cenacastelo.vai()
 historia()
