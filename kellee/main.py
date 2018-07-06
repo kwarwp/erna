6df4fb86d87d4443d8e3bea4b3f6b9e411c924ed
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-06 10:24:57 -0300

    Saved kellee/main.py at Timestamp: 2018-07-06 13:24:56

--- kellee/main.py
+++ kellee/main.py
@@ -1,9 +1,10 @@
 from _spy.vitollino.main import Cena, Elemento, Texto
 #Matheus
 FLORESTA = "http://2.bp.blogspot.com/-QfJCgBGjGLI/U7sgKKLbRWI/AAAAAAAAZEE/RXU-xgjdPlg/s1600/ZQHFqWr.jpg"
-hokage = "https://i.pinimg.com/originals/39/02/44/390244b135c00088e1b8b7871d45957a.png"
+linkhokage = "https://i.pinimg.com/originals/39/02/44/390244b135c00088e1b8b7871d45957a.png"
 #Olá eu sou o kakashi, o sexto hokage da aldeia da folha
 def historia ():
      cenafloresta = Cena(img = "http://2.bp.blogspot.com/-QfJCgBGjGLI/U7sgKKLbRWI/AAAAAAAAZEE/RXU-xgjdPlg/s1600/ZQHFqWr.jpg")
-     cenafloresta.vai()
+     hokage = Elemento(img = linkhokage, tit = "Hokage", style = dict (top = 60, left = 150, height = 200 , width = 60))
+cenafloresta.vai()
 historia()
