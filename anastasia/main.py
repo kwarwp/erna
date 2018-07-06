1aec5c3e081ff36c582bac816c8aade490d4c4b1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-06 11:26:46 -0300

    Saved anastasia/main.py at Timestamp: 2018-07-06 14:26:46

--- anastasia/main.py
+++ anastasia/main.py
@@ -7,7 +7,7 @@ def historia():
    cenaoceano = Cena(img = "https://mardehistorias.files.wordpress.com/2010/11/oceano.jpg")
    nemo = Elemento(img="http://www.araguaiaaquarios.com.br/loja/modules/possequence/images/image_1.jpg",
                   tit="nemo",
-                  style = dict (top = 150, left = 60, height = 60, width = 200))
+                  style = dict (top = 180, left = 40, height = 40, width = 170))
    nemo.entra(cenaoceano)
    cenaoceano.vai()
 historia()
