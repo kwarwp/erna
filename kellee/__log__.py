8dde0ebd5c13cf1fc4aa50867503623209f7dd7b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-06 10:56:34 -0300

    Saved kellee/__log__.py at Timestamp: 2018-07-06 13:56:34

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -143,3 +143,21 @@ NameError: name 'Cenafloresta' is not defined
     txthokage = Texto(Cenafloresta, "Ola eu sou o kakashi, o sexto hokage da aldeia da folha")
 NameError: name 'Cenafloresta' is not defined
 '''},
+{'date': 'Fri Jul 06 2018 10:56:35.72 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 13
+    historia()
+  module <module> line 9
+    txthokage = Texto(Cenafloresta, "Olá eu sou o kakashi, o sexto hokage da aldeia da folha")
+NameError: name 'Cenafloresta' is not defined
+'''},
