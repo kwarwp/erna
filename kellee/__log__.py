3d9f17970631a2db379c791520b14f9c4c49e608
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-06 10:40:26 -0300

    Saved kellee/__log__.py at Timestamp: 2018-07-06 13:40:26

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -91,3 +91,19 @@ NameError: name 'cenafloresta' is not defined
     hokage.entrar(cenafloresta)
 AttributeError: 'Elemento' object has no attribute 'entrar'
 '''},
+{'date': 'Fri Jul 06 2018 10:40:26.865 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 9
+    cenafloresta.vai()
+NameError: name 'cenafloresta' is not defined
+'''},
