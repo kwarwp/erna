8b9115942a67178628f49f1eea60e25724b94b75
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-06 10:42:58 -0300

    Saved kellee/main.py at Timestamp: 2018-07-06 13:42:58

--- kellee/main.py
+++ kellee/main.py
@@ -5,7 +5,7 @@ linkhokage = "https://i.pinimg.com/originals/39/02/44/390244b135c00088e1b8b7871d
 #Olá eu sou o kakashi, o sexto hokage da aldeia da folha
 def historia ():
      cenafloresta = Cena(img = "http://2.bp.blogspot.com/-QfJCgBGjGLI/U7sgKKLbRWI/AAAAAAAAZEE/RXU-xgjdPlg/s1600/ZQHFqWr.jpg")
-     hokage = Elemento(img = linkhokage, tit = "Hokage", style = dict (top = 60, left = 150, height = 200 , width = 60))
+     hokage = Elemento(img = linkhokage, tit = "Hokage", style = dict (top = 60, left = 150, height = 350 , width = 60))
      hokage.entra(cenafloresta)
      cenafloresta.vai()
 historia()
