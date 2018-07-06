d732f52e8d41e0352a096d1e46bde99ee22af94d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-06 10:40:00 -0300

    Saved sara/__log__.py at Timestamp: 2018-07-06 13:39:59

--- sara/__log__.py
+++ sara/__log__.py
@@ -42,3 +42,10 @@ TypeError: 'module' object is not callable
                                                                                                                        ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Fri Jul 06 2018 10:40:01.575 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 8
+  carro = Elemento(img = linkcarro, tit = "Carro", style = dict (top = 250, left = 150, height = 200, width = 10
+                                                                                                                       ^
+SyntaxError: invalid syntax
+'''},
