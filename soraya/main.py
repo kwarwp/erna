b0b093841df4a81e66afe1118dd98fa220af49c2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-06 10:44:48 -0300

    Saved soraya/main.py at Timestamp: 2018-07-06 13:44:47

--- soraya/main.py
+++ soraya/main.py
@@ -6,7 +6,7 @@ linkaviao = "http://i.imgur.com/geBGkhx.png"
 #Help!!!
 def historia():
      cenacidade = Cena(img = "https://media.wsimag.com/attachments/87d7e431ded78e4f143c7e84db3e01a694a8276e/store/fill/1020/574/cc1c81d29f191e0dd08e953179b0d2d3baa88ab791485adf5c60ca97fdd2/Chongqing-China-Tempestade-sobre-a-cidade.jpg")
-     aviao = Elemento(img = linkaviao,tit = "Aviao", style = dict (top = 500, left = 150, height = 60, width = 60))
+     aviao = Elemento(img = linkaviao,tit = "Aviao", style = dict (top = 200, left = 150, height = 60, width = 60))
      aviao.entra(cenacidade)
      cenacidade.vai()
 historia()
