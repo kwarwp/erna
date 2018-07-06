3f55b15b25722ce7558e3e62aa1d24278ce3e32a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-06 10:39:25 -0300

    Saved libby/main.py at Timestamp: 2018-07-06 13:39:25

--- libby/main.py
+++ libby/main.py
@@ -5,7 +5,7 @@ linkpassaros = "https://i.pinimg.com/originals/18/80/5f/18805f2560e462573fee515c
 #piupiupiupiu
 def historia():
      Cenalugar = Cena(img = "https://media-cdn.tripadvisor.com/media/photo-s/12/f8/68/3d/big-bus-paris-hop-on.jpg")   
-     passaros = Elemento(img = linkpassaros, tit = "passaros", style = dict (top =  100,left =  150 ,height = 100, width = 60))
+     passaros = Elemento(img = linkpassaros, tit = "passaros", style = dict (top =  100,left =  150 ,height = 100, width = 100))
      passaros.entra(Cenalugar)
      Cenalugar.vai()
 historia()
