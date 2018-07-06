2cdf315e1309b5c255f06ae65072c7ae5b06f82e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-06 10:52:50 -0300

    Saved sara/__log__.py at Timestamp: 2018-07-06 13:52:50

--- sara/__log__.py
+++ sara/__log__.py
@@ -63,3 +63,21 @@ SyntaxError: invalid syntax
                                ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Fri Jul 06 2018 10:52:51.658 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 12
+    historia()
+  module <module> line 9
+    txtcarro = Texto(cenaCastelo, "cheguei")
+NameError: name 'cenaCastelo' is not defined
+'''},
