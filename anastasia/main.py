7c4b115bef2a7bdbe2756bffdee194c9c34c6327
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-06 11:57:53 -0300

    Saved anastasia/main.py at Timestamp: 2018-07-06 14:57:53

--- anastasia/main.py
+++ anastasia/main.py
@@ -9,5 +9,8 @@ def historia():
                   tit="nemo",
                   style = dict (top = 180, left = 40, height = 40, width = 170))
    nemo.entra(cenaoceano)
+   txtnemo = texto(cenaoceano,
+                  "Glub Glub"
+   nemo.vai = txtnemo.vai
    cenaoceano.vai()
 historia()
