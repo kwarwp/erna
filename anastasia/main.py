3a901d6351d63adf0a0cfc39b2c9917880b764e7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-06 11:35:23 -0300

    Saved anastasia/main.py at Timestamp: 2018-07-06 14:35:22

--- anastasia/main.py
+++ anastasia/main.py
@@ -4,7 +4,7 @@ oceano = "https://mardehistorias.files.wordpress.com/2010/11/oceano.jpg"
 nemo = "http://www.araguaiaaquarios.com.br/loja/modules/possequence/images/image_1.jpg"
 #Glub Glub
 def historia():
-   cenaoceano = Cena(img = "https://mardehistorias.files.wordpress.com/2010/11/oceano.jpg")
+   cenaoceano = Cena(img="https://mardehistorias.files.wordpress.com/2010/11/oceano.jpg")              
    nemo = Elemento(img="http://www.araguaiaaquarios.com.br/loja/modules/possequence/images/image_1.jpg",
                   tit="nemo",
                   style = dict (top = 180, left = 40, height = 40, width = 170))
