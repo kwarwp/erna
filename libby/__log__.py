e742f4981c331ac67e5ac9e802572a6a507149d7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-06 10:11:45 -0300

    Created libby/__log__.py at Timestamp: 2018-07-06 13:11:45

---
+++ libby/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Fri Jul 06 2018 10:11:43.620 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 7
+  Cenalugar = Cena(img = "https://media-cdn.tripadvisor.com/media/photo-s/12/f8/68/3d/big-bus-paris-hop-on.jpg" 
+                                                                                                                       ^
+SyntaxError: invalid syntax
+'''},
