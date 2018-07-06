6857c0d1d26f15a2828eac14bb4b1eb36307afef
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-06 10:36:52 -0300

    Saved stacy/main.py at Timestamp: 2018-07-06 13:36:52

--- stacy/main.py
+++ stacy/main.py
@@ -6,6 +6,6 @@ linkparis = "http://www.freepngimg.com/download/paris/24125-5-paris-picture.png"
 def historia():
      cenaceu = Cena(img = "https://png.pngtree.com/thumb_back/fw800/back_pic/03/56/29/24579dee6c24b45.jpg")
      paris = Elemento(img = linkparis, tit = 'Paris', style = dict (top = 60, left = 150, height = 200, width = 60))
-     paris.entra(cenaParis)
+     paris.entra(cenaCeu)
      cenaceu.vai()
 historia()
