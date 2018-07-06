afab31d77836eddbac0cfbe1cbcfab63400a3d86
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-06 10:24:57 -0300

    Saved libby/main.py at Timestamp: 2018-07-06 13:24:57

--- libby/main.py
+++ libby/main.py
@@ -1,9 +1,10 @@
 from _spy.vitollino.main import Cena, Elemento, Texto
 # Lana
 lugar = "https://media-cdn.tripadvisor.com/media/photo-s/12/f8/68/3d/big-bus-paris-hop-on.jpg"
-passaros = "https://i.pinimg.com/originals/18/80/5f/18805f2560e462573fee515cfe9eb364.jpg"
+linkpassaros = "https://i.pinimg.com/originals/18/80/5f/18805f2560e462573fee515cfe9eb364.jpg"
 #piupiupiupiu
 def historia():
-     Cenalugar = Cena(img = "https://media-cdn.tripadvisor.com/media/photo-s/12/f8/68/3d/big-bus-paris-hop-on.jpg")
-     Cenalugar.vai
+     Cenalugar = Cena(img = "https://media-cdn.tripadvisor.com/media/photo-s/12/f8/68/3d/big-bus-paris-hop-on.jpg")   
+     passaros = Elemento(img = linkpassaros, tit = "passaros", style = dict (top =  60,left =  150 ,height = 200, width = 60))
+     Cenalugar.vai()
 historia()
