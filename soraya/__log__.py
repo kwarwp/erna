d424eabafd1458c3960987e3b1698641b4806562
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-06 10:53:37 -0300

    Created soraya/__log__.py at Timestamp: 2018-07-06 13:53:37

---
+++ soraya/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Fri Jul 06 2018 10:53:38.286 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 10
+  txtaviao = Texto(cenacidade, "help”)
+                                      ^
+SyntaxError: EOL while scanning string literal
+'''},
