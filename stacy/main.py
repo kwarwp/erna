195117daf61ca23029855b60477a6ff939c584c3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-06 10:24:59 -0300

    Saved stacy/main.py at Timestamp: 2018-07-06 13:24:59

--- stacy/main.py
+++ stacy/main.py
@@ -1,9 +1,10 @@
 from _spy.vitollino.main import Cena, Elemento, Texto
 #Tassiane 
 ceu = "https://png.pngtree.com/thumb_back/fw800/back_pic/03/56/29/24579dee6c24b45.jpg"
-paris = "http://www.freepngimg.com/download/paris/24125-5-paris-picture.png"
+linkparis = "http://www.freepngimg.com/download/paris/24125-5-paris-picture.png"
 #Bonne nuit
 def historia():
      cenaceu = Cena(img = "https://png.pngtree.com/thumb_back/fw800/back_pic/03/56/29/24579dee6c24b45.jpg")
-     cenaceu.vai()
+  paris = Elemento(img = linkparis, tit = 'Paris', style = dict (top = 60, left = 150, height = 200, width = 60))
+  cenaceu.vai()
 historia()
