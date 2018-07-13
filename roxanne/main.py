da99aab39ad63e030af57d286203c9f01a9a3daf
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-13 10:46:25 -0300

    Saved roxanne/main.py at Timestamp: 2018-07-13 13:46:24

--- roxanne/main.py
+++ roxanne/main.py
@@ -5,7 +5,7 @@ ferro = "https://png.pngtree.com/element_pic/16/12/27/f8d3f37ac40656a40836942a56
 #um carro bateu em um pilar de ferro e esposa ficou muito triste
 def historia():
        cenaTristreza = Cena(img = "http://clinicaintervir.com.br/wp-content/uploads/2018/01/imagem-depress%C3%A3o-750x480.jpg")
-       nomeferro = Elemento(img = "https://png.pngtree.com/element_pic/16/12/27/f8d3f37ac40656a40836942a56f8fa53.jpg", tit = "Destruiçaõ", style = dict (top =60, left =150, height =,200width =60))
-      cenaTristreza.vai()
+       nomeferro = Elemento(img = "https://png.pngtree.com/element_pic/16/12/27/f8d3f37ac40656a40836942a56f8fa53.jpg", tit = "Destruiçaõ", style = dict (top =60, left =150, height =200, width =60))
+       cenaTristreza.vai()
       
 historia()
