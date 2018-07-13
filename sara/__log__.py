43f60bde04c15f00e7e1cde0eaa4f7b823109753
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-13 11:47:15 -0300

    Saved sara/__log__.py at Timestamp: 2018-07-13 14:47:15

--- sara/__log__.py
+++ sara/__log__.py
@@ -160,3 +160,21 @@ NameError: name 'linkcarro' is not defined
     transparente = Elemento(img = linkcarro, tit = "Carro", style = dict (top = 220, left = 150, height = 200, width = 60))
 NameError: name 'linkcarro' is not defined
 '''},
+{'date': 'Fri Jul 13 2018 11:47:13.997 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 13
+    historia()
+  module <module> line 8
+    transparente = Elemento(img = linkcarro, tit = "transparente", style = dict (top = 220, left = 150, height = 200, width = 60))
+NameError: name 'linkcarro' is not defined
+'''},
