cda054b44b4f7dfe19003fa3fe8b650d94d9984b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-13 10:25:45 -0300

    Saved roxanne/main.py at Timestamp: 2018-07-13 13:25:45

--- roxanne/main.py
+++ roxanne/main.py
@@ -4,6 +4,6 @@ tristeza = "http://clinicaintervir.com.br/wp-content/uploads/2018/01/imagem-depr
 ferro = "https://png.pngtree.com/element_pic/16/12/27/f8d3f37ac40656a40836942a56f8fa53.jpg"
 #um carro bateu em um pilar de ferro e esposa ficou muito triste
 def historia():
-       cenatristreza = Cena(img = "http://f.i.uol.com.br/fotografia/2016/05/02/606604-970x600-1.jpg")
-       cenatristreza.vai()
+       cenaTristreza = Cena(img = "http://clinicaintervir.com.br/wp-content/uploads/2018/01/imagem-depress%C3%A3o-750x480.jpg")
+       cenaTristreza.vai()
 historia()
