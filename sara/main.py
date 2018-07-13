4a4bdffd1309e8539844d1dc82566cf1b81a0656
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-13 11:38:24 -0300

    Saved sara/main.py at Timestamp: 2018-07-13 14:38:23

--- sara/main.py
+++ sara/main.py
@@ -6,7 +6,7 @@ linkcarro = "http://www.vpowerautomoveis.com.br/home/imgs/carro_top4.png"
 def historia():
      cenaambiente7 = Cena(img = "https://i.imgur.com/YEk25am.png")
      carro = Elemento(img = linkcarro, tit = "Carro", style = dict (top = 220, left = 150, height = 200, width = 60))
-     txtcarro = Texto(cenaambiente7, "cheguei")
+     txtcarro = Texto(cenaambiente7, "turn left and go up the ramp")
      carro.vai = txtcarro.vai
      carro.entra(cenaambiente7)
      cenaambiente7.vai()
