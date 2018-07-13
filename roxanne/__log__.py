60533a8023a8eb7a4d7713cba8f3af60eb4a0e9b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-13 10:17:44 -0300

    Created roxanne/__log__.py at Timestamp: 2018-07-13 13:17:44

---
+++ roxanne/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Fri Jul 13 2018 10:17:42.241 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 4
+  ferro "https://png.pngtree.com/element_pic/16/12/27/f8d3f37ac40656a40836942a56f8fa53.jpg"
+         ^
+SyntaxError: invalid syntax
+'''},
