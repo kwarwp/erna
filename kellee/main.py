11219b6a0cfcd9f595ddf4963b8f3f4d8c0d7aa7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-13 11:45:12 -0300

    Saved kellee/main.py at Timestamp: 2018-07-13 14:45:12

--- kellee/main.py
+++ kellee/main.py
@@ -1,13 +1,13 @@
 from _spy.vitollino.main import Cena, Elemento, Texto
 #Matheus
-FLORESTA = "http://2.bp.blogspot.com/-QfJCgBGjGLI/U7sgKKLbRWI/AAAAAAAAZEE/RXU-xgjdPlg/s1600/ZQHFqWr.jpg"
+ambiente2 = "http://2.bp.blogspot.com/-QfJCgBGjGLI/U7sgKKLbRWI/AAAAAAAAZEE/RXU-xgjdPlg/s1600/ZQHFqWr.jpg"
 linkhokage = "https://i.pinimg.com/originals/39/02/44/390244b135c00088e1b8b7871d45957a.png"
 #Olá eu sou o kakashi, o sexto hokage da aldeia da folha
 def historia ():
-     cenafloresta = Cena(img = "http://2.bp.blogspot.com/-QfJCgBGjGLI/U7sgKKLbRWI/AAAAAAAAZEE/RXU-xgjdPlg/s1600/ZQHFqWr.jpg")
+     cenaambiente2 = Cena(img = "http://2.bp.blogspot.com/-QfJCgBGjGLI/U7sgKKLbRWI/AAAAAAAAZEE/RXU-xgjdPlg/s1600/ZQHFqWr.jpg")
      hokage = Elemento(img = linkhokage, tit = "Hokage", style = dict (top = 60, left = 160, height = 800, width = 60))
-     txthokage = Texto(cenafloresta, "Olá!!! Eu sou o kakashi, o sexto hokage da aldeia da folha")
+     txthokage = Texto(cenaambiente2, "Olá!!! Eu sou o kakashi, o sexto hokage da aldeia da folha")
      hokage.vai = txthokage.vai
-     hokage.entra(cenafloresta)
-     cenafloresta.vai()
+     hokage.entra(cenaambiente2)
+     cenaambiente2.vai()
 historia()
