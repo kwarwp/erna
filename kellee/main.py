53d197f2624a18550667a288e9c05d58c47f2e21
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-13 11:47:19 -0300

    Saved kellee/main.py at Timestamp: 2018-07-13 14:47:18

--- kellee/main.py
+++ kellee/main.py
@@ -4,7 +4,7 @@ ambiente2 = "https://i.imgur.com/l1LT0JE.png"
 linkhokage = "https://i.pinimg.com/originals/39/02/44/390244b135c00088e1b8b7871d45957a.png"
 #Olá eu sou o kakashi, o sexto hokage da aldeia da folha
 def historia ():
-     cenaambiente2 = Cena(img = "http://2.bp.blogspot.com/-QfJCgBGjGLI/U7sgKKLbRWI/AAAAAAAAZEE/RXU-xgjdPlg/s1600/ZQHFqWr.jpg")
+     cenaambiente2 = Cena(img = "https://i.imgur.com/l1LT0JE.png")
      hokage = Elemento(img = linkhokage, tit = "Hokage", style = dict (top = 60, left = 160, height = 800, width = 60))
      txthokage = Texto(cenaambiente2, "Olá!!! Eu sou o kakashi, o sexto hokage da aldeia da folha")
      hokage.vai = txthokage.vai
