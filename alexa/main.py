76eb58c0ecd8887b9a31780677b79956b2e45063
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-13 11:26:42 -0300

    Saved alexa/main.py at Timestamp: 2018-07-13 14:26:41

--- alexa/main.py
+++ alexa/main.py
@@ -1,10 +1,10 @@
 from _spy.vitollino.main import Cena, Elemento, Texto
 #Alexandre
-praia = "https://beachpark.com.br/mobile/wp-content/themes/beachpark/img/praia/apresentacao.jpg"
+AMBIENTE3 = "https://i.imgur.com/rkX57B2.png"
 linkpessoa = "http://www.trackandfield.com.br/wordpress/wp-content/uploads/2014/10/Screen-Shot-2014-10-15-at-4.38.02-PM.png"
 # bom dia 
 def historia():
-     cenapraia = Cena(img = "https://beachpark.com.br/mobile/wp-content/themes/beachpark/img/praia/apresentacao.jpg")
+     AMBIENTE3 = Cena(img = "https://i.imgur.com/rkX57B2.png")
      pessoa = Elemento(img = linkpessoa, tit = "Pessoa", style = dict (top = 60, left = 150, height = 200, width = 60))
      txtpessoa = Texto(cenapraia,"Bom Dia")
      pessoa.vai = txtpessoa.vai
