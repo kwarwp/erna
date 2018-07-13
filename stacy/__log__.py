b6599071d7790b50963fa8e03fade858573f4e8c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-13 11:54:48 -0300

    Saved stacy/__log__.py at Timestamp: 2018-07-13 14:54:48

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -429,3 +429,21 @@ SyntaxError: EOL while scanning string literal
     txtparis = Texto(cenaambiente4, "A esquerda, temos o espaço game com computadores e videogames: playstation 4  e xboxone.")
 NameError: name 'cenaambiente4' is not defined
 '''},
+{'date': 'Fri Jul 13 2018 11:54:47.497 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 13
+    historia()   
+  module <module> line 9
+    txtparis = Texto(cenaambiente4 , "A esquerda, temos o espaço game com computadores e videogames: playstation 4  e xboxone.")
+NameError: name 'cenaambiente4' is not defined
+'''},
