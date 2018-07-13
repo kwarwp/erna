5570261d351831ec40cae49634f01fadd8dff304
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-13 11:26:28 -0300

    Saved roxanne/__log__.py at Timestamp: 2018-07-13 14:26:28

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -121,3 +121,21 @@ AttributeError: 'Elemento' object has no attribute 'entrar'
     nomeferro.entrar(cenatristeza)
 AttributeError: 'Elemento' object has no attribute 'entrar'
 '''},
+{'date': 'Fri Jul 13 2018 11:26:27.562 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 12
+    historia()
+  module <module> line 9
+    ferro.entrar(cenatristeza)
+NameError: name 'ferro' is not defined
+'''},
