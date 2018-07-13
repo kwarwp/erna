3f1d0266a0c3aeb322e274b41295208f90b39db1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-13 11:50:20 -0300

    Saved soraya/main.py at Timestamp: 2018-07-13 14:50:19

--- soraya/main.py
+++ soraya/main.py
@@ -7,7 +7,8 @@ linkaviao = "http://3.bp.blogspot.com/-4SsFci7nC_Q/VmsgYxu3EdI/AAAAAAAAPzo/1pjiU
 def historia():
      cenaambiente9= Cena(img = "https://i.imgur.com/38Sq4Jm.png")
      aviao = Elemento(img = linkaviao,tit = "Aviao", style = dict (top = 260, left = 150, height = 60, width = 60))
-     txtaviao = Texto(ambiente9, "Help")
+     txtaviao = Texto(ambiente9, "Feel free to those a computer. The teacher is on her way"
+
      aviao.vai=txtaviao.vai
      aviao.entra(ambiente9)
      cenaambiente9.vai()
