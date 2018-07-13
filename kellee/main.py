f04bcfd28c2fd9e9e3fc89a792830bdc3f88ac0b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-13 11:53:42 -0300

    Saved kellee/main.py at Timestamp: 2018-07-13 14:53:40

--- kellee/main.py
+++ kellee/main.py
@@ -6,7 +6,7 @@ linkhokage = "http://3.bp.blogspot.com/-4SsFci7nC_Q/VmsgYxu3EdI/AAAAAAAAPzo/1pji
 def historia ():
      cenaambiente2 = Cena(img = "https://i.imgur.com/l1LT0JE.png")
      hokage = Elemento(img = linkhokage, tit = "Hokage", style = dict (top = 60, left = 160, height = 800, width = 60))
-     txthokage = Texto(cenaambiente2, "Olá!!! Eu sou o kakashi, o sexto hokage da aldeia da folha")
+     txthokage = Texto(cenaambiente2, "Go straight until you get to the front door.")
      hokage.vai = txthokage.vai
      hokage.entra(cenaambiente2)
      cenaambiente2.vai()
