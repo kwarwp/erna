9ae80457a959d3abc86aeb8a5768d042cb2536eb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-13 11:32:03 -0300

    Saved soraya/main.py at Timestamp: 2018-07-13 14:32:03

--- soraya/main.py
+++ soraya/main.py
@@ -1,11 +1,11 @@
 # erna.soraya.main.py
 from _spy.vitollino.main import Cena, Elemento, Texto
 #renata
-cidade = "https://media.wsimag.com/attachments/87d7e431ded78e4f143c7e84db3e01a694a8276e/store/fill/1020/574/cc1c81d29f191e0dd08e953179b0d2d3baa88ab791485adf5c60ca97fdd2/Chongqing-China-Tempestade-sobre-a-cidade.jpg"
+ambiente9 = "https://i.imgur.com/38Sq4Jm.png"
 linkaviao = "http://i.imgur.com/geBGkhx.png"
 #Help!!!
 def historia():
-     cenacidade = Cena(img = "https://media.wsimag.com/attachments/87d7e431ded78e4f143c7e84db3e01a694a8276e/store/fill/1020/574/cc1c81d29f191e0dd08e953179b0d2d3baa88ab791485adf5c60ca97fdd2/Chongqing-China-Tempestade-sobre-a-cidade.jpg")
+     cenaambiente9= Cena(img = "https://i.imgur.com/38Sq4Jm.png")
      aviao = Elemento(img = linkaviao,tit = "Aviao", style = dict (top = 260, left = 150, height = 60, width = 60))
      txtaviao = Texto(cenacidade, "Help")
      aviao.vai=txtaviao.vai
