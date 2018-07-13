f41dedad25434efbf3e347a97a5a4b7b2b9db344
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-13 11:31:52 -0300

    Saved sara/main.py at Timestamp: 2018-07-13 14:31:52

--- sara/main.py
+++ sara/main.py
@@ -1,10 +1,10 @@
 from _spy.vitollino.main import Cena, Elemento, Texto
 # Luciane
-castelo = "https://upload.wikimedia.org/wikipedia/commons/8/88/Castelo_de_Guimar%C3%A3es_%28Portugal%29.jpg"
+ambiente7 = "https://i.imgur.com/YEk25am.png"
 linkcarro = "http://www.vpowerautomoveis.com.br/home/imgs/carro_top4.png"
 # cheguei
 def historia():
-     cenacastelo = Cena(img = "https://upload.wikimedia.org/wikipedia/commons/8/88/Castelo_de_Guimar%C3%A3es_%28Portugal%29.jpg")
+     cenaambiente7 = Cena(img = "https://i.imgur.com/YEk25am.png")
      carro = Elemento(img = linkcarro, tit = "Carro", style = dict (top = 220, left = 150, height = 200, width = 60))
      txtcarro = Texto(cenacastelo, "cheguei")
      carro.vai = txtcarro.vai
