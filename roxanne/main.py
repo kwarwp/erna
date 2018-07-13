bf35d007f9d5342e7c33f8a832f0687dedf0a17f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-13 10:50:35 -0300

    Saved roxanne/main.py at Timestamp: 2018-07-13 13:50:35

--- roxanne/main.py
+++ roxanne/main.py
@@ -1,7 +1,7 @@
 from _spy.vitollino.main import Cena, Elemento, Texto
 #Breno celso
 tristeza = "http://clinicaintervir.com.br/wp-content/uploads/2018/01/imagem-depress%C3%A3o-750x480.jpg"
-ferro = "https://png.pngtree.com/element_pic/16/12/27/f8d3f37ac40656a40836942a56f8fa53.jpg"
+nomeferro = "https://png.pngtree.com/element_pic/16/12/27/f8d3f37ac40656a40836942a56f8fa53.jpg"
 #um carro bateu em um pilar de ferro e esposa ficou muito triste
 def historia():
        cenaTristreza = Cena(img = "http://clinicaintervir.com.br/wp-content/uploads/2018/01/imagem-depress%C3%A3o-750x480.jpg")
