86baa7b596abd9f7f19e7a0d50116f8c32c4d920
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-13 11:36:42 -0300

    Saved soraya/__log__.py at Timestamp: 2018-07-13 14:36:42

--- soraya/__log__.py
+++ soraya/__log__.py
@@ -99,3 +99,23 @@ AttributeError: 'str' object has no attribute 'nome'
     self.scorer.update(valor=cena.nome, move=self.xy,
 AttributeError: 'str' object has no attribute 'nome'
 '''},
+{'date': 'Fri Jul 13 2018 11:36:42.818 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 14
+    historia()
+  module <module> line 12
+    aviao.entra(ambiente9)
+  module _spy.vitollino.main line 453
+    self.scorer.update(valor=cena.nome, move=self.xy,
+AttributeError: 'str' object has no attribute 'nome'
+'''},
