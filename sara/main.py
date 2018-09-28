c0bc278a4331276fcfa71d4aabdcfa9218a40ebc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-28 07:42:29 -0300

    Saved sara/main.py at Timestamp: 2018-09-28 10:42:29

--- sara/main.py
+++ sara/main.py
@@ -1,13 +1,13 @@
-from _spy.vitollino.main import Cena, Elemento, Texto
+#from _spy.vitollino.main import Cena, Elemento, Texto
 # Luciane
-ambiente7 = "https://i.imgur.com/YEk25am.png"
-linkcarro = "http://s.glbimg.com/po/tt/f/original/2012/11/05/fundo_transparente.png"
+#ambiente7 = "https://i.imgur.com/YEk25am.png"
+#linkcarro = "http://s.glbimg.com/po/tt/f/original/2012/11/05/fundo_transparente.png"
 # cheguei
-def historia():
-     cenaambiente7 = Cena(img = "https://i.imgur.com/YEk25am.png")
-     carro = Elemento(img = linkcarro, tit = "Carro", style = dict (top = 220, left = 150, height = 200, width = 60))
-     txtcarro = Texto(cenaambiente7, "turn left and go up the ramp")
-     carro.vai = txtcarro.vai
-     carro.entra(cenaambiente7)
-     cenaambiente7.vai()
-historia()
+#def historia():
+#     cenaambiente7 = Cena(img = "https://i.imgur.com/YEk25am.png")
+#     carro = Elemento(img = linkcarro, tit = "Carro", style = dict (top = 220, left = 150, height = 200, width = 60))
+#     txtcarro = Texto(cenaambiente7, "turn left and go up the ramp")
+#     carro.vai = txtcarro.vai
+#     carro.entra(cenaambiente7)
+#     cenaambiente7.vai()
+#historia()
