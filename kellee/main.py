5d5b02d2368e7b776001086760767db0fa5f2ae6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-28 07:44:19 -0300

    Saved kellee/main.py at Timestamp: 2018-09-28 10:44:19

--- kellee/main.py
+++ kellee/main.py
@@ -1,13 +1,13 @@
-from _spy.vitollino.main import Cena, Elemento, Texto
+#from _spy.vitollino.main import Cena, Elemento, Texto
 #Matheus
-ambiente2 = "https://i.imgur.com/l1LT0JE.png"
-linkhokage = "http://3.bp.blogspot.com/-4SsFci7nC_Q/VmsgYxu3EdI/AAAAAAAAPzo/1pjiUbLTJSs/s1600/Estrela%2B5.png"
+#ambiente2 = "https://i.imgur.com/l1LT0JE.png"
+#linkhokage = "http://3.bp.blogspot.com/-4SsFci7nC_Q/VmsgYxu3EdI/AAAAAAAAPzo/1pjiUbLTJSs/s1600/Estrela%2B5.png"
 #Olá eu sou o kakashi, o sexto hokage da aldeia da folha
-def historia ():
-     cenaambiente2 = Cena(img = "https://i.imgur.com/l1LT0JE.png")
-     hokage = Elemento(img = linkhokage, tit = "Hokage", style = dict (top = 60, left = 160, height = 800, width = 60))
-     txthokage = Texto(cenaambiente2, "Go straight until you get to the front door.")
-     hokage.vai = txthokage.vai
-     hokage.entra(cenaambiente2)
-     cenaambiente2.vai()
-historia()
+#def historia ():
+#     cenaambiente2 = Cena(img = "https://i.imgur.com/l1LT0JE.png")
+#     hokage = Elemento(img = linkhokage, tit = "Hokage", style = dict (top = 60, left = 160, height = 800, width = 60))
+#     txthokage = Texto(cenaambiente2, "Go straight until you get to the front door.")
+#     hokage.vai = txthokage.vai
+#     hokage.entra(cenaambiente2)
+#     cenaambiente2.vai()
+#historia()
