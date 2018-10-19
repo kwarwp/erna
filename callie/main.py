f9158396a726d7fb72144cada81387b85a4e1019
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-19 11:47:04 -0300

    Saved callie/main.py at Timestamp: 2018-10-19 14:47:04

--- callie/main.py
+++ callie/main.py
@@ -1,11 +1,11 @@
-# Leandro
+#bruno
 from _spy.vitollino.main import Cena, Elemento, Texto
 
-ZUMBI ="http://www.deezer-blog.com/assets/http://www.deezer-blog.com/assets/sites/8/2015/10/zumbiblog.pngsites/8/2015/10/zumbiblog.png"
+ZUMBI ="https://images.vexels.com/media/users/3/137945/isolated/preview/fbeb505577b4a8b8b87018f254d43c7e--cone-do-transporte-de-barco-by-vexels.png"
 
 def Historia():
     ILHA = Cena(img ="https://ogimg.infoglobo.com.br/in/21993934-fe0-499/FT1086A/652/x06_10_2017.-Jornal-O-Globo.-Ilha-Grande-4.jpg.pagespeed.ic.NPxcsQST9i.jpg")
     zumbi9= Elemento(img=ZUMBI,tit="ZUMBI",style=dict(left=150, top=60, width=60,height=200))
     zumbi9.entra(ILHA)
     ILHA.vai ()
-   Historia()
+Historia()
