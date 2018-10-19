a11bf44465208fe959d37d837845684adb7f2957
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-19 11:31:24 -0300

    Saved callie/main.py at Timestamp: 2018-10-19 14:31:23

--- callie/main.py
+++ callie/main.py
@@ -1,4 +1,4 @@
-# Leandro
+#bruno
 from _spy.vitollino.main import Cena, Elemento, Texto
 
 ZUMBI ="http://www.deezer-blog.com/assets/http://www.deezer-blog.com/assets/sites/8/2015/10/zumbiblog.pngsites/8/2015/10/zumbiblog.png"
