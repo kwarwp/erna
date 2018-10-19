c2dbd8527c5ea32321d25e71720b95794553edca
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-19 11:59:28 -0300

    Saved ruzwana/main.py at Timestamp: 2018-10-19 14:59:28

--- ruzwana/main.py
+++ ruzwana/main.py
@@ -1,10 +1,11 @@
 #Feliphe
 from _spy.vitollino.main import Cena, Elemento, Texto
 
-ZUMBI ="http://www.deezer-blog.com/assets/http://www.deezer-blog.com/assets/sites/8/2015/10/zumbiblog.pngsites/8/2015/10/zumbiblog.png"
+ZUMBI ="https://cdn.pixabay.com/photo/2017/01/09/14/59/zombie-1966751_960_720.png"
 
 def Historia():
-    ILHA = Cena(img ="https://ogimg.infoglobo.com.br/in/21993934-fe0-499/FT1086A/652/x06_10_2017.-Jornal-O-Globo.-Ilha-Grande-4.jpg.pagespeed.ic.NPxcsQST9i.jpg")
-    zumbi9= Elemento(img=ZUMBI,tit="ZUMBI",style=dict(left=150, top=60, width=60,height=200))
-    ILHA.vai ()
+    CEMITERIO = Cena(img ="http://www.jomagazin.cz/wp-content/uploads/2015/01/angel-cemetery-cross-fog-gothic-favim.com-263560.jpg")
+    zumbi = Elemento(img =ZUMBI,tit="Zumbi",style=dict(left = 180, top = 40, heigth = 40, width = 170)
+    zumbi.entra(CEMITERIO)
+    CEMITERIO.vai ()
 Historia()
