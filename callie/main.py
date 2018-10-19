f5f1bc5cc1fb3f0fe7384640a04ff53208080fcb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-19 11:27:03 -0300

    Created callie/main.py at Timestamp: 2018-10-19 14:27:03

---
+++ callie/main.py
@@ -0,0 +1,10 @@
+#bruno
+from _spy.vitollino.main import Cena, Elemento, Texto
+
+ZUMBI ="http://www.deezer-blog.com/assets/http://www.deezer-blog.com/assets/sites/8/2015/10/zumbiblog.pngsites/8/2015/10/zumbiblog.png"
+
+def Historia():
+    ILHA = Cena(img ="https://ogimg.infoglobo.com.br/in/21993934-fe0-499/FT1086A/652/x06_10_2017.-Jornal-O-Globo.-Ilha-Grande-4.jpg.pagespeed.ic.NPxcsQST9i.jpg")
+    zumbi9= Elemento(img=ZUMBI,tit="ZUMBI",style=dict(left=150, top=60, width=60,height=200))
+    ILHA.Vai ()
+Historia()
