90042def851e0c0781c9abd92d9414dda43a675f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-19 11:55:18 -0300

    Created callie/__log__.py at Timestamp: 2018-10-19 14:55:18

---
+++ callie/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Fri Oct 19 2018 11:55:16.315 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 11
+  Historia()
+  ^
+IndentationError: unexpected indent
+'''},
