1483c2f7c985936e00b340cc9a19b1fa9edc9fd5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-19 11:27:02 -0300

    Created parisa/main.py at Timestamp: 2018-10-19 14:27:02

---
+++ parisa/main.py
@@ -0,0 +1,10 @@
+# Jacqueline
+from _spy.vitollino.main import Cena, Elemento, Texto
+
+LEAO = "http://lionsbrusquebercofiacao.org.br/wp-content/uploads/2015/12/logo-leoes.png"
+
+def Historia():
+    MONTANHA = Cena(img = "https://www.fundoswiki.com/Uploads/fundoswiki.com/ImagensGrandes/paisagem-montanha-9.jpg") 
+    leao = Elemento(img=LEAO, tit="leao", style=dict(left=150, top=60, width=60, heigth=200)
+    MONTANHA.vai()
+Historia()
