6cc6e11711ca0e40b466b9997fc9614b13158166
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-19 11:50:19 -0300

    Saved naomi/main.py at Timestamp: 2018-10-19 14:50:19

--- naomi/main.py
+++ naomi/main.py
@@ -1,10 +1,11 @@
 # Cibele
 from _spy.vitollino.main import Cena, Elemento, Texto
 
-ZUMBI ="http://www.deezer-blog.com/assets/http://www.deezer-blog.com/assets/sites/8/2015/10/zumbiblog.pngsites/8/2015/10/zumbiblog.png"
+ZUMBI ="https://images.vexels.com/media/users/3/149936/isolated/preview/a169be67e61672671368d23ac8db8b17-sereia-posar-silueta-by-vexels.png"
 
 def Historia():
     ILHA = Cena(img ="https://ogimg.infoglobo.com.br/in/21993934-fe0-499/FT1086A/652/x06_10_2017.-Jornal-O-Globo.-Ilha-Grande-4.jpg.pagespeed.ic.NPxcsQST9i.jpg")
     zumbi9= Elemento(img=ZUMBI,tit="ZUMBI",style=dict(left=150, top=60, width=60,height=200))
+    zumbi9.entra(ILHA)
     ILHA.vai ()
 Historia()
