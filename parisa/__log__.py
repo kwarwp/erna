9a9c2b0242f43c32784e966338026762e94389ab
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-19 11:53:39 -0300

    Created parisa/__log__.py at Timestamp: 2018-10-19 14:53:39

---
+++ parisa/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Fri Oct 19 2018 11:53:36.207 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 10
+  ILHA.vai ()
+  ^
+IndentationError: unexpected indent
+'''},
