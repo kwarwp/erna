a7b76d0492f5ed59c5f68d717cf05e37f54c9620
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-19 11:31:17 -0300

    Saved parisa/main.py at Timestamp: 2018-10-19 14:31:17

--- parisa/main.py
+++ parisa/main.py
@@ -1,8 +1,10 @@
 # Jacqueline
 from _spy.vitollino.main import Cena, Elemento, Texto
-LEAO = "http://lionsbrusquebercofiacao.org.br/wp-content/uploads/2015/12/logo-leoes.png"
+
+ZUMBI ="http://www.deezer-blog.com/assets/http://www.deezer-blog.com/assets/sites/8/2015/10/zumbiblog.pngsites/8/2015/10/zumbiblog.png"
+
 def Historia():
-    MONTANHA = Cena(img = "https://www.fundoswiki.com/Uploads/fundoswiki.com/ImagensGrandes/paisagem-montanha-9.jpg") 
-    leao = Elemento(img=LEAO, tit="leao", style=dict(left=150, top=60, width=60, heigth=200)
-    MONTANHA.vai()
+    ILHA = Cena(img ="https://ogimg.infoglobo.com.br/in/21993934-fe0-499/FT1086A/652/x06_10_2017.-Jornal-O-Globo.-Ilha-Grande-4.jpg.pagespeed.ic.NPxcsQST9i.jpg")
+    zumbi9= Elemento(img=ZUMBI,tit="ZUMBI",style=dict(left=150, top=60, width=60,height=200))
+    ILHA.vai ()
 Historia()
