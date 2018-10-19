2e7cb7d01d271327ae50b9b847472473f5ab8a10
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-19 11:43:13 -0300

    Saved callie/main.py at Timestamp: 2018-10-19 14:43:12

--- callie/main.py
+++ callie/main.py
@@ -6,5 +6,6 @@ ZUMBI ="http://www.deezer-blog.com/assets/http://www.deezer-blog.com/assets/site
 def Historia():
     ILHA = Cena(img ="https://ogimg.infoglobo.com.br/in/21993934-fe0-499/FT1086A/652/x06_10_2017.-Jornal-O-Globo.-Ilha-Grande-4.jpg.pagespeed.ic.NPxcsQST9i.jpg")
     zumbi9= Elemento(img=ZUMBI,tit="ZUMBI",style=dict(left=150, top=60, width=60,height=200))
+    zumbi9.entra(ILHA)
     ILHA.vai ()
 Historia()
