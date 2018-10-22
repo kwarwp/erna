af3257e01ca337bbdff43e714590e1c0a72598f5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-22 17:49:53 -0300

    Created morgan/__log__.py at Timestamp: 2018-10-22 20:49:53

---
+++ morgan/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Oct 22 2018 17:49:50.485 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 6
+  Goku = Elemento(img,
+  ^
+IndentationError: unexpected indent
+'''},
