c570d65fc2aab40115fd00045163546bb3addb03
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-22 18:10:36 -0300

    Saved tracy/main.py at Timestamp: 2018-10-22 21:10:36

--- tracy/main.py
+++ tracy/main.py
@@ -1,1 +1,14 @@
 # DANIEL
+from _spy.vitollino.main import Cena, Elemento,Texto
+
+CACADOR="https://2.bp.blogspot.com/-proH1UNc9Yw/V1ShJQ0HsMI/AAAAAAAAGF4/dSBPvEvXwDYFgEi1jLNlIcnc6JwKhmtawCLcB/s1600/o-ca%25C3%25A7ador-e-a-rainha-do-gelo-poshhterk.png"
+def Historia():
+    cenaFloresta=Cena(img="http://homeroreis.com/wp-content/uploads/2015/10/floresta.jpg")
+    cacador = Elemento(img= CACADOR,
+              tit ="Caçador",
+              style = dict(left=150, top=60, width=60, height=200))
+    cacador.entra(cenaFloresta)
+    txtcacador = Texto (cenaFloresta, "Hello")
+    txtcacador.vai = cacador.vai
+    cenaFloresta.vai()
+Historia()
