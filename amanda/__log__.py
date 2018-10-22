4538372c98e683429e9832d030c35ff6cca60be3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-22 18:00:20 -0300

    Created amanda/__log__.py at Timestamp: 2018-10-22 21:00:19

---
+++ amanda/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Oct 22 2018 18:00:18.61 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 6
+  goku = Elemento(img=GOKU,
+  ^
+IndentationError: unexpected indent
+'''},
