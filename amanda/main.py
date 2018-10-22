718a82bd7ebf291bcabc17f6bc6b9836e555c964
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-22 18:29:13 -0300

    Saved amanda/main.py at Timestamp: 2018-10-22 21:29:13

--- amanda/main.py
+++ amanda/main.py
@@ -2,7 +2,8 @@
 from _spy.vitollino.main import Cena, Elemento, Texto
 
 GOKU ="https://i.ytimg.com/vi/jEjtNGZnsak/hqdefault.jpg"
-cenaTeatro = Cena(img ="http://1.bp.blogspot.com/-aGwacYjRKw8/T-St9kEVvMI/AAAAAAAAAvs/ximTg6O8XUA/s1600/ESCENARIOS29+%282%.jpg")
+def Historia():
+    cenaTeatro = Cena(img ="http://1.bp.blogspot.com/-aGwacYjRKw8/T-St9kEVvMI/AAAAAAAAAvs/ximTg6O8XUA/s1600/ESCENARIOS29+%282%.jpg")
     goku = Elemento(img=GOKU,
            tit ="Goku",
            style = dict(left=150, top=60,width=60, height=200))
