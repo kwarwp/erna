bc81125235d26ebc424ac1f52267f412491cc1aa
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-22 18:20:33 -0300

    Saved amanda/__log__.py at Timestamp: 2018-10-22 21:20:33

--- amanda/__log__.py
+++ amanda/__log__.py
@@ -13,3 +13,10 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Mon Oct 22 2018 18:20:33.193 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 6
+  goku = Elemento(img=GOKU,
+  ^
+IndentationError: unexpected indent
+'''},
