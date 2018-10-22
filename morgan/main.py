98d44527075ebfc2cb874497e4aba3d7e594799d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-22 17:35:25 -0300

    Saved morgan/main.py at Timestamp: 2018-10-22 20:35:25

--- morgan/main.py
+++ morgan/main.py
@@ -1,1 +1,7 @@
 # mariahduarte
+from _spy.vitollino.main import Cena,Elemento,Texto
+PERSONAGEM ="https://www.google.com.br/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjmwdSo2PfdAhVtuVkKHdWDAMoQjRx6BAgBEAU&url=http%3A%2F%2Flife-writer.tumblr.com%2Fpost%2F123761609710%2Fi-missed-my-kids&psig=AOvVaw1kWh3XpDoI0P2G30m_UH6o&ust=1539116915250785.png"
+def Historia():
+    cenaSea = Cena(img = "https://www.google.com.br/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiHltyB3vfdAhWOzlkKHR49B8gQjRx6BAgBEAU&url=http%3A%2F%2Fpt-br.contos-de-hora-de-aventura.wikia.com%2Fwiki%2FArquivo%3A294392_Papel-de-Parede-Castelo-Doce-Hora-da-Aventura_1920x1080.jpg&psig=AOvVaw3Csk-jnuzvEvpqP_P6cRev&ust=1539118413301038")
+    cenaSea.vai
+Historia()
