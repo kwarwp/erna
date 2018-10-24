1b9a295453dcde5727894981c4644f43ee0ebaca
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-24 18:29:23 -0300

    Saved tracy/__log__.py at Timestamp: 2018-10-24 21:29:23

--- tracy/__log__.py
+++ tracy/__log__.py
@@ -13,3 +13,10 @@ SyntaxError: invalid syntax
                                                                                                                    ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Oct 24 2018 18:29:22.740 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 7
+  cacador = Elemento(img= CACADOR,
+                                                                                                                   ^
+SyntaxError: invalid syntax
+'''},
