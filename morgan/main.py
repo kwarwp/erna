08f848a32020ecb2255bcda8ae520b06363d4165
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-24 17:20:11 -0300

    Saved morgan/main.py at Timestamp: 2018-10-24 20:20:11

--- morgan/main.py
+++ morgan/main.py
@@ -3,12 +3,12 @@ from _spy.vitollino.main import Cena, Elemento, Texto
 
 GOKU ="https://i.ytimg.com/vi/jEjtNGZnsak/hqdefault.jpg"
 def Historia():
-    cenaTeatro = Cena(img ="http://1.bp.blogspot.com/-aGwacYjRKw8/T-St9kEVvMI/AAAAAAAAAvs/ximTg6O8XUA/s1600/ESCENARIOS29+%282%.jpg")
+    cenaTeatro = Cena(img ="http://1.bp.blogspot.com/-aGwacYjRKw8/T-St9kEVvMI/Avs/ximTg6O8XUA/s1600/ESCENARIOS29+%282%.jpg")
     goku = Elemento(img= GOKU,
            tit ="Goku",
            style = dict(left=150, top=60,width=60, height=200))
     goku.entra(cenaTeatro)
     txtGoKu=Texto (cenaTeatro, "Hello")
-    goku.vai = txtGoku.vai
+    goku=GoKutxt
     cenaTeatro.vai()
 Historia()
