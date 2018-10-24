5bb78fc44ca38ff9a58d4beca22115855035e624
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-24 17:24:43 -0300

    Saved morgan/__log__.py at Timestamp: 2018-10-24 20:24:43

--- morgan/__log__.py
+++ morgan/__log__.py
@@ -186,3 +186,21 @@ NameError: name 'GoKu' is not defined
     goKu.vai=goKu.vaitxt
 NameError: name 'goKu' is not defined
 '''},
+{'date': 'Wed Oct 24 2018 17:24:43.45 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 14
+    Historia()
+  module <module> line 12
+    cenagoKu.vai=goKu.txt
+NameError: name 'cenagoKu' is not defined
+'''},
