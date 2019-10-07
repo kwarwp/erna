c86f57057a955e90a12a7ffa2203d6d651aa138b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-06 21:25:55 -0300

    Saved callie/main.py @2019-10-07 00:25

--- callie/main.py
+++ callie/main.py
@@ -1,11 +1,1 @@
-# Leandro
-from _spy.vitollino.main import Cena, Elemento, Texto
-
-ZUMBI ="http://pngimage.net/wp-content/uploads/2018/06/pequena-sereia-personagens-png-1.png"
-
-def Historia():
-    ILHA = Cena(img ="https://ogimg.infoglobo.com.br/in/21993934-fe0-499/FT1086A/652/x06_10_2017.-Jornal-O-Globo.-Ilha-Grande-4.jpg.pagespeed.ic.NPxcsQST9i.jpg")
-    zumbi9= Elemento(img=ZUMBI,tit="ZUMBI",style=dict(left=150, top=60, width=60,height=200))
-    zumbi9.entra(ILHA)
-    ILHA.vai()
-Historia()
+# erna.callie.main.py
