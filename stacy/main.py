eda28bebb6e2fc1c97913bd0cfa945770c5363a6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-06 21:22:59 -0300

    Saved stacy/main.py @2019-10-07 00:22

--- stacy/main.py
+++ stacy/main.py
@@ -1,13 +1,1 @@
-from _spy.vitollino.main import Cena, Elemento, Texto
-#Tassiane 
-ambiente4 = "https://i.imgur.com/Ei24vcD.png"
-linkparis = "http://www.freepngimg.com/download/paris/24125-5-paris-picture.png"
-#Bonne nuit
-def historia():
-     ambiente4 = Cena(img = "https://i.imgur.com/Ei24vcD.png")
-     paris = Elemento(img = linkparis, tit = 'Paris', style = dict (top = 50, left = 30, height = 50, width = 40))
-     txtparis = Texto(cenaambiente4, "A esquerda, temos o espaço game com computadores e videogames: playstation 4  e xboxone.")
-     paris.vai = txtparis.vai
-     paris.entra(ambiente4)
-     ambiente4.vai()
-historia()   
+# erna.stacy.main.py
