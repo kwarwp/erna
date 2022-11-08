bb9c808c8530964792ec68e968e717422fc1331c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-11-08 10:57:28 -0300

    Saved sara/__log__.py @2022-11-08 13:57

--- sara/__log__.py
+++ sara/__log__.py
@@ -196,3 +196,19 @@ NameError: name 'linkcarro' is not defined
     transparente = Elemento(img = linkcarro, tit = "transparente", style = dict (top = 220, left = 150, height = 200, width = 60))
 NameError: name 'linkcarro' is not defined
 '''},
+{'date': 'Tue Nov 08 2022 10:57:19.825 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 4
+    Cena1=Cena(CenaDoMazePark)
+NameError: name 'CenaDoMazePark' is not defined
+'''},
