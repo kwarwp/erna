f89501104240afde47f3dc696c21c3d32af04029
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-11-08 14:36:57 -0300

    Saved naomi/__log__.py @2022-11-08 17:36

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -110,3 +110,19 @@ Traceback (most recent call last):
   module <module> line 2
     from _spy.vitollino.main import Cema
 '''},
+{'date': 'Tue Nov 08 2022 14:36:47.938 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 4
+    padaria=Cema(pao)
+NameError: name 'Cema' is not defined
+'''},
