849dcac060eb8a9071013d2fa788bfbeff45173f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-10 13:03:57 -0300

    Created courtney/__log__.py @2023-10-10 16:03

---
+++ courtney/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Oct 10 2023 13:04:03.765 GMt-0700 (Horário de Verão do Pacífico) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 41
+  """Representa a classe principal do jogo
+  ^
+IndentationError: expected an indented block
+'''},
