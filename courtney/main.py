253fefc08e9de976282e68d9eb76dec6a43529f0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-10 12:08:11 -0300

    Saved courtney/main.py @2023-10-10 15:08

--- courtney/main.py
+++ courtney/main.py
@@ -1,12 +1,34 @@
 # erna.courtney.main.py
+#__author__glaucio
+
+""" Página de entrada do jogo Ilha Proibida.
+..codeauthor:: Glaucio <glaucio75@ufrj.br>
+
+Changelog
+---------
+..versionadded:: 23.10
+    Classes Ilha, Terreno, Peao(10).
+
+..versionadded:: 23.09
+    Versão Inicial (26).
+    
+"""
+    
 from _spy.vitollino.main import Cena, Elemento,STYLE
+from julia.main import IlhaProibida as Ilha
+from julia import main as jmain
+from anastasia import main as amain
 STYLE["width"] = 800
+STYLE["height"] = "600px"
 IMAGEM = "https://imgur.com/gVHmY2v.jpg"
 PORTAO_BRONZE = "https://imgur.com/BL6lB7H.jpg" 
 PALACIO_CORAL = "https://imgur.com/tLDbzd2.jpg"
 PAWN = "https://imgur.com/zO3kiRp.png"
 
-
+#class IlhaProibida(jmain.IlhaProibida):
+#class IlhaProibida(Ilha):
+"""essas chamadas de parâmetros é para herdar as classes que estão em outros códigos que eu quero usar 
+"""
 class IlhaProibida:
     def __init__(self):
         oceano = Cena(IMAGEM).vai()
@@ -18,10 +40,14 @@ class IlhaProibida:
 
   
 class Peao:
+    """Marcador usado para definir a posição do jogador nos terrenos.
+    """
+    
     def __init__(self, oceano):
         self.peao = Elemento(PAWN, x=20, y=20, w=80, h=80, cena=oceano, vai=self.move)
     
     def move(self, ev=None):
         self.peao.x = 150
         
-IlhaProibida()
+if __name__== "__main__"
+    #IlhaProibida()
